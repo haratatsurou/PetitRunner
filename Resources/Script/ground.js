5d87ba5f6705b3c240836ff29d9bfa0b4a8e2b10
haratatsurou <hara.tatsurou@gmail.com>
2016-08-05 18:55:34 +0900

    床の設置

---
+++ Resources/Script/ground.js
@@ -0,0 +1,42 @@
+﻿//
+// ### SmileBoom Petit Developer：Gadget Template Script ###
+//
+//   クラス名：ground
+// 親クラス名：Sbt.Gadget
+//
+// コンテキストメニュー（右クリックで開くメニュー）でタッチや描画用のメソッドが追加できます。
+//
+
+//
+// ### 定数 ###
+//
+
+//
+// ### コンストラクタ ###
+//
+var ground = function() {};
+
+//
+// ### 継承 ###
+//
+ground.prototype = new Sbt.Gadget();
+
+//
+// ### 初期化処理 ###
+//
+ground.prototype.doInitialize = function() {};
+
+//
+// ### 毎フレームごとの処理 ###
+//
+ground.prototype.doUpdate = function() {
+    // 継承元GadgetのdoUpdate()呼び出し
+    Sbt.Gadget.prototype.doUpdate.call(this);
+    this.location[0] -= 6;
+    if (this.location[0] < -184) {
+        this.location[0] = 1150;
+    }
+    // ※この下にプログラムを追加してください
+};
+
+// ※この下に独自処理を追加できます
