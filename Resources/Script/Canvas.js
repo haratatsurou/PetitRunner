2ccb28afc8c809090224e850c7fa3d9455ac1896
haratatsurou <hara.tatsurou@gmail.com>
2016-08-05 18:55:34 +0900

    床の設置

--- Resources/Script/Canvas.js
+++ Resources/Script/Canvas.js
@@ -10,12 +10,13 @@
 //
 // ### 定数 ###
 //
-
+var MODE_START = 0; //ゲームスタート 
+var MODE_MAIN = 1; // ゲームメイン 
+var MODE_END = 2; // ゲーム終了
 //
 // ### コンストラクタ ###
-//
-var Canvas = function() {
-};
+//   
+var Canvas = function() {};
 
 //
 // ### 継承 ###
@@ -26,27 +27,33 @@ Canvas.prototype = new Sbt.Canvas();
 // ### 初期化処理 ###
 //
 Canvas.prototype.doInitialize = function() {
-	// [event setup start] Gadgetのイベント初期化([event setup end]まで編集・削除禁止)
-	// [event setup end]
-	
-	// ※Gadget初期化前に必要な処理はこの下に追加してください
-	
-	// Canvasに配置されている全てのGadget初期化
-	Sbt.Canvas.prototype.doInitialize.call( this );
-	
-	// ※Gadget初期化後に必要な処理はこの下に追加してください
+    // [event setup start] Gadgetのイベント初期化([event setup end]まで編集・削除禁止)
+    // [event setup end]
+
+    // ※Gadget初期化前に必要な処理はこの下に追加してください
+
+    // Canvasに配置されている全てのGadget初期化
+    Sbt.Canvas.prototype.doInitialize.call(this);
+
+    // ※Gadget初期化後に必要な処理はこの下に追加してください
+    Sbt.global.score = 0;
+    this.arrayTiles = [];
 };
 
 //
 // ### 毎フレームごとの処理 ###
 //
 Canvas.prototype.doUpdate = function() {
-	// 継承元CanvasのdoUpdate()呼び出し
-	Sbt.Canvas.prototype.doUpdate.call( this );
-	
-	// ※この下にプログラムを追加してください
+    // 継承元CanvasのdoUpdate()呼び出し
+    Sbt.Canvas.prototype.doUpdate.call(this);
+
+    // ※この下にプログラムを追加してください
 };
 
-// [event function] Gadgetのイベントメソッドを追加(この行の編集・削除禁止)
+Canvas.prototype.hall = function(index) {
 
-// ※この下に独自処理を追加できます
+    this.arrayTiles.splice(index, 1);
+};
+Canvas.prototype.createTile = function(gadget) {
+    this.arrayTiles.push(gadget);
+};
--- Resources/Script/player.js
+++ Resources/Script/player.js
@@ -14,8 +14,7 @@
 //
 // ### コンストラクタ ###
 //
-var player = function() {
-};
+var player = function() {};
 
 //
 // ### 継承 ###
@@ -26,36 +25,60 @@ player.prototype = new Sbt.Gadget();
 // ### 初期化処理 ###
 //
 player.prototype.doInitialize = function() {
-	this.setAnimation("default");
+    this.setAnimation("default");
 };
 
 //
 // ### 毎フレームごとの処理 ###
 //
 player.prototype.doUpdate = function() {
-	// 継承元GadgetのdoUpdate()呼び出し
-	Sbt.Gadget.prototype.doUpdate.call( this );
-		if(this.animation.id=="default"){
-			return;
-	}
-		if (this.animation.isEnd) {
-			if(this.animation.id=="jump_up"){
-				this.setAnimation("jump_down");
-			}else{
-				this.setAnimation("default");
-			}
-	}
+    // 継承元GadgetのdoUpdate()呼び出し
+    Sbt.Gadget.prototype.doUpdate.call(this);
+    if (this.animation.id == "default") {
+        return;
+    }
+    if (this.animation.isEnd) {
+        if (this.animation.id == "jump_up") {
+            this.setAnimation("jump_down");
+        } else {
+            this.setAnimation("default");
+        }
+    }
+    this.Landing();
 
-	// ※この下にプログラムを追加してください
+    // ※この下にプログラムを追加してください
 };
 
-player.prototype.doMouseUp = function( location, id ) {
-	if(this.animation.id=="default"){
-		this.setAnimation("jump_up");
-	}
-	else if(this.animation.id=="jump_up"){
-		this.setAnimation("doublejump");
-	}
-	
-	return true; // true：以降のCanvas/Gadgetは処理されません
+player.prototype.doMouseUp = function(location, id) {
+    if (this.animation.id == "default") {
+        this.setAnimation("jump_up");
+    } else if (this.animation.id == "jump_up") {
+        this.setAnimation("doublejump");
+    }
+
+    return true; // true：以降のCanvas/Gadgetは処理されません
+};
+
+//着地の判定をとる関数
+player.prototype.Landing = function() {
+    // var hit = this.animation.findAnimation("default");
+    // if (hit == null) return;
+
+    // var playerLeft = this.location[0] + hit.drawOffset[0] - hit.width / 2;
+    var playerTop = this.location[1] - this.height / 2;
+    // var playerRight = playerLeft + hit.width;
+    var playerBottom = playerTop + this.height;
+
+    var Tiles = this.canvas.arrayTiles;
+
+    for (var i = Tiles.length - 1; i >= 0; i--) {
+        var tile = this.Tiles[i];
+
+        var tileTop = tile.location[0] - tile.height / 2;
+        var tileButtom = tileTop + tile.height;
+        if (playerBottom === tileTop) {
+            alert("やったぜ");
+        }
+    }
+
 };
